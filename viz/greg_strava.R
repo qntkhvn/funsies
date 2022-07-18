2429e193801d6c0b8e4d14222a558aa7b0b86ea6
Quang Nguyen <43119108+qntkhvn@users.noreply.github.com>
2022-07-18 23:27:23 +0700

    Create greg_strava.R

---
+++ viz/greg_strava.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(rStrava)
+library(lubridate)
+
+healthygreg <- athl_fun(34707096)
+
+healthygreg |> 
+  pluck("34707096") |> 
+  pluck("monthly") |> 
+  mutate(month = month(month, label = TRUE)) |> 
+  ggplot(aes(month, miles, group = 1)) +
+  geom_point(aes(size = miles)) +
+  geom_line()
